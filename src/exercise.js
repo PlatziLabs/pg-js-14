e66ed5b86d9d4cd3436c3f605ba672652bfafe11
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-07 05:31:45 -0500

    save

--- src/exercise.js
+++ src/exercise.js
@@ -1,3 +1,10 @@
 export function fibonacci(number) {
-  // Tu código aquí 👈
+  const fibonacciArray = [0, 1];
+
+  for (let i = 2; i <= number; i++){
+    fibonacciArray.push(fibonacciArray[i - 2] + fibonacciArray[i - 1]);
+  }
+  fibonacciArray.shift(0);
+  return fibonacciArray;
 }
+
